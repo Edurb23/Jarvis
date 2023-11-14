5ee9684f069d5f281d58eefb80eaac76c2aff82f
Edurb23 <rm551987@fiap.com.br>
2023-11-14 18:25:18 -0300

    Inicando o projeto

---
+++ src/components/Cabecalho/cabecalho.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import "./cabecalho.scss"
+
+
+export default function Cabecalho() {
+  return (
+   <>
+    <header className='cabecalho'>
+        <Link href="/">
+
+        <Image src= "/image/logo.png" 
+            width={220}
+            height={220}
+            alt ="Imagem da logo da porto seguro"
+            className='logo'
+            />
+          </Link>
+
+          <nav>
+            <ul>
+                <li>
+                <Link href="/" className='a'>Login</Link>
+                </li>
+                <li>
+                <Link href="/" className='a'>Tratamento</Link>
+                </li>
+                <li>
+                <Link href="/" className='a'>Exames</Link>
+                </li>
+                <li>
+                <Link href="/" className='a'>Medicamentos</Link>
+                </li>
+              
+            </ul>
+          </nav>
+
+        
+    </header>
+   </>
+  )
+}
