0a5c59e0d2e8b31568eab867cc6e1ec9c20bcc72
Edurb23 <rm551987@fiap.com.br>
2023-11-15 19:37:15 -0300

    fazendo banner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,18 @@
 import Conteudo from "@/components/Conteudo/conteudo"
+import Image from 'next/image'
 
 export default function Home() {
   return (
   <>
+    <section className="banner">
+        <Image
+        src= "/image/banner-jarvis.png" 
+        width={100}
+        height={100}
+        alt ="banner"
+        className='bannerImage'
+        />
+</section>
     <Conteudo/>
   </>
   )
