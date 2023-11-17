c3184be4e50d39d4a5309e26127b3cf63d610e25
silvio009 <rm551987@fiap.com.br>
2023-11-17 18:40:21 -0300

    mexendo no banner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,13 +6,9 @@ export default function Home() {
   return (
   <>
     <section className="banner">
-        <Image
-        src= "/image/banner-exame.png" 
-        width={500}
-        height={600}
-        alt ="banner"
-        className='bannerImage'
-        />
+        <div className="banner">
+
+        </div>
 </section>
     <Conteudo/>
   </>
