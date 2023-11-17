c0887f40751192bc3ff296b3b3909ea911d152cf
silvio009 <rm550821@fiap.com.br>
2023-11-17 18:55:31 -0300

    mexendo no banner

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,22 @@
 import Conteudo from "@/components/Conteudo/conteudo"
-import Image from 'next/image'
+import Image from "next/image"
+
 import './home.scss'
 
 export default function Home() {
   return (
-  <>
+    <>
     <section className="banner">
-        <div className="banner">
-
-        </div>
+        <Image
+        src= "/image/banner-jarvis.png" 
+        width={200}
+        height={400}
+        alt ="banner"
+        className='bannerImage'
+        />
 </section>
     <Conteudo/>
   </>
+
   )
 }
