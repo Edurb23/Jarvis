f29f9425717ba37aed31bae23e97fa9f9833730d
leonardobarral <leonardobarral2@gmail.com>
2023-11-22 22:19:54 -0300

    finalizando a api tipo GET

--- src/app/conta/[id]/page.jsx
+++ src/app/conta/[id]/page.jsx
@@ -2,20 +2,21 @@
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import './conta.scss'
+import { useRouter } from 'next/navigation';
 
-export default function Conta(params) {
+export default function Conta({params}) {
   
-   const router = setRouter();
+   const router = useRouter();
 
    const [paciente, setPaciente] = useState({
-    "nm_cliente":"",
+    "nm_paciente":"",
     "nr_cpf":"",
     "nr_rg":"",
     "fl_sexo_biologico": "",
     "nr_altura": "",
     "nr_peso": "",
     "dt_data_nascimento":"",
-    "id_cliente": params.id
+    "id_paciente": params.id
    })
 
 
@@ -29,10 +30,11 @@ export default function Conta(params) {
          }
        });
        let paciente = await responseget.json();
+       console.log(paciente)
        setPaciente(paciente);
       }catch(erro){
        console.log(erro);
-       redirect("/error")
+      // redirect("/error")
       }
     };
     getPacientes();
@@ -58,41 +60,39 @@ export default function Conta(params) {
           <form>
             <div className='nome'>
               <label for="idNome">Nome</label>
-              <input type="text" name='nome' id='idNome' />
+              <div>
+                <span>{paciente.nm_paciente}</span>
+              </div>
             </div>
             <div className='cpf'>
               <label for="idCpf">CPF</label>
-              <input type="text" id="cpf" name="idCpf"  />
+              <div>
+                <span>{paciente.nr_cpf}</span>
+              </div>
             </div>
             <div className='rg'>
               <label for="idRg">RG</label>
-              <input type="text" id="rg" name="idRg"  />
+              <div>
+                <span>{paciente.nr_rg}</span>
+              </div>
             </div>
             <div className='sexoBiologico'>
               <label for="sexoBiologicoID">Sexo Biológico:</label>
-              <select id="sexoBiologico" name="sexoBiologicoID" >
-                <option value="masculino">Masculino</option>
-                <option value="feminino">Feminino</option>
-                <option value="outro">Outro</option>
-              </select>
+              <div>
+                <span>{paciente.fl_sexo_biologico}</span>
+              </div>
             </div>
             <div className='altura'>
               <label for="alturaID">Altura (cm):</label>
-              <input type="number" id="altura" name="alturaId" />
+              <div>
+                <span>{paciente.nr_altura}</span>
+              </div>
             </div>
             <div className='peso'>
               <label for="pesoID">Peso (kg):</label>
-              <input type="number" id="peso" name="peso"  />
-            </div>
-
-
-            <div className='email'>
-              <label for="idEmail">Endereço de email</label>
-              <input type="email" name='email' id='idEmail'  />
-            </div>
-            <div className='senha'>
-              <label for="idSenha">Senha</label>
-              <input type="password" name='senha' id='idSenha'  />
+              <div>
+                <span>{paciente.nr_peso}</span>
+              </div>
             </div>
             <button className='botaoEditar'><Link className='link-editar'  href="/conta/editar" >Editar</Link></button>
             <button className='botaoExcluir'><Link className='link-excluir'  href="/" >Excluir Conta</Link></button>
