b366963cf9b0abb7464a1583ba13d06b507071e2
leonardobarral <leonardobarral2@gmail.com>
2023-11-22 22:42:47 -0300

    get funcional

--- src/app/conta/editar/[id]/page.jsx
+++ src/app/conta/editar/[id]/page.jsx
@@ -1,10 +1,8 @@
 "use client";
-
-
-import React from 'react'
 import Link from 'next/link'
 import './editarConta.scss'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
 export default function EditarConta({params}) {
   
@@ -31,24 +29,30 @@ export default function EditarConta({params}) {
 }, [msgstatus])
 
 useEffect( () => {
-  const getPacientes = async () => {
+  const obeterPaciente = async() => {
     try{
-     const responseget = await fetch(`http://localhost:8080/api/paciente/${params.id}`,{
-       method: "GET",
-       headers:{
-         "Content-Type": "application/json"
-       }
-     });
-     let paciente = await responseget.json();
-     console.log(paciente)
-     setPaciente(paciente);
-    }catch(erro){
-     console.log(erro);
-    // redirect("/error")
+      const responseget = await fetch(`http://localhost:8080/api/paciente/${params.id}`,{
+        method: "GET",
+        headers:{
+          "Content-Type":"application/json" 
+        }
+      });
+      let pacienteAtual = await responseget.json();
+      setPaciente(pacienteAtual)
+    }catch(error){
+      console.log(error);
+
     }
-  };
-  getPacientes();
- }, [params.id])
+  }
+  obeterPaciente()
+}, [paciente.id]
+)
+
+
+ const handleChange = (e)=>{
+  const {name, value} = e.target;
+  setPaciente({...paciente,[name]:value})
+}
 
   
   
@@ -67,15 +71,15 @@ useEffect( () => {
           <form>
             <div className='nome'>
               <label for="idNome">Nome</label>
-              <input type="text" name='nome' id='idNome'  />
+              <input type="text" name='nome' id='idNome' value={paciente.nm_paciente}  onChange={handleChange}/>
             </div>
             <div className='cpf'>
               <label for="idCpf">CPF</label>
-              <input type="text" id="cpf" name="idCpf"  />
+              <input type="text" id="cpf" name="idCpf" value={paciente.nm_paciente}  />
             </div>
             <div className='rg'>
               <label for="idRg">RG</label>
-              <input type="text" id="rg" name="idRg" />
+              <input type="text" id="rg" name="idRg" value={paciente.nm_paciente}  />
             </div>
             <div className='sexoBiologico'>
               <label for="sexoBiologicoID">Sexo Biológico:</label>
@@ -87,11 +91,11 @@ useEffect( () => {
             </div>
             <div className='altura'>
               <label for="alturaID">Altura (cm):</label>
-              <input type="number" id="altura" name="alturaId" />
+              <input type="number" id="altura" name="alturaId" value={paciente.nm_paciente} />
             </div>
             <div className='peso'>
               <label for="pesoID">Peso (kg):</label>
-              <input type="number" id="peso" name="peso" />
+              <input type="number" id="peso" name="peso" value={paciente.nm_paciente}/>
             </div>
 
 
@@ -99,10 +103,7 @@ useEffect( () => {
               <label for="idEmail">Endereço de email</label>
               <input type="email" name='email' id='idEmail'  />
             </div>
-            <div className='senha'>
-              <label for="idSenha">Senha</label>
-              <input type="password" name='senha' id='idSenha' />
-            </div>
+      
             <button className='botaoEditar'><Link className='link-editar'  href="/" >Editar</Link></button>
             <button className='botaoEditar'><Link className='link-editar'  href="/conta" >Voltar</Link></button>
           </form>
