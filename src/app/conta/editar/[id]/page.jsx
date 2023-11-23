c969e4438689359dad8fca1ce7f7e2cfe410b403
leonardobarral <leonardobarral2@gmail.com>
2023-11-23 17:42:03 -0300

    get funcionando

--- src/app/conta/editar/[id]/page.jsx
+++ src/app/conta/editar/[id]/page.jsx
@@ -54,6 +54,32 @@ useEffect( () => {
   setPaciente({...paciente,[name]:value})
 }
 
+const [email, setEmail] = useState({
+  "ds_email": "",
+  "st_email": "",
+  "id_paciente": params.id,
+  "id_email": params.id
+
+})
+
+useEffect(() => {
+  const obeterEmail = async() => {
+    try{
+      const responseget = await fetch(`http://localhost:8080/api/emailpaciente/${params.id}`,{
+        method: "GET",
+        headers:{
+          "Content-Type":"application/json" 
+        }
+      });
+      let emailAtual = await responseget.json();
+      setEmail(emailAtual)
+    }catch(error){
+      console.log(error)
+    }
+  }
+  obeterEmail()
+}, [email.id])
+
   
   
   
@@ -75,15 +101,15 @@ useEffect( () => {
             </div>
             <div className='cpf'>
               <label for="idCpf">CPF</label>
-              <input type="text" id="cpf" name="idCpf" value={paciente.nm_paciente}  />
+              <input type="text" id="cpf" name="idCpf" value={paciente.nr_cpf}  />
             </div>
             <div className='rg'>
               <label for="idRg">RG</label>
-              <input type="text" id="rg" name="idRg" value={paciente.nm_paciente}  />
+              <input type="text" id="rg" name="idRg" value={paciente.nr_rg}  />
             </div>
             <div className='sexoBiologico'>
               <label for="sexoBiologicoID">Sexo Biológico:</label>
-              <select id="sexoBiologico" name="sexoBiologicoID" >
+              <select id="sexoBiologico" name="sexoBiologicoID" value={paciente.fl_sexo_biologico}>
                 <option value="masculino">Masculino</option>
                 <option value="feminino">Feminino</option>
                 <option value="outro">Outro</option>
@@ -91,17 +117,17 @@ useEffect( () => {
             </div>
             <div className='altura'>
               <label for="alturaID">Altura (cm):</label>
-              <input type="number" id="altura" name="alturaId" value={paciente.nm_paciente} />
+              <input type="number" id="altura" name="alturaId" value={paciente.nr_altura} />
             </div>
             <div className='peso'>
               <label for="pesoID">Peso (kg):</label>
-              <input type="number" id="peso" name="peso" value={paciente.nm_paciente}/>
+              <input type="number" id="peso" name="peso" value={paciente.nr_peso}/>
             </div>
 
 
             <div className='email'>
               <label for="idEmail">Endereço de email</label>
-              <input type="email" name='email' id='idEmail'  />
+              <input type="email" name='email' id='idEmail' value={email.ds_email}  />
             </div>
       
             <button className='botaoEditar'><Link className='link-editar'  href="/" >Editar</Link></button>
