7b3480ab0580fcce2cfd999d52fb8f4dab966b9d
leonardobarral <leonardobarral2@gmail.com>
2023-11-23 21:40:40 -0300

    Modificação na página de cadastro

--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -19,7 +19,20 @@ export default function Cadastro() {
     "nr_altura": "",
     "nr_peso": "",
     "dt_data_nascimento": "",
-    
+  })
+
+  const [email, setEmail] = useState({
+    "ds_email": "",
+    "st_email": "",
+    "id_paciente": params.id,
+    "id_email": ""
+  })
+
+  const [senha, setSenha] = useState({
+    "id_cadastro": "",
+    "id_paciente": params.id,
+    "id_email": "",
+    "cd_senha": ""
   })
 
   useEffect(() => {
@@ -27,10 +40,24 @@ export default function Cadastro() {
       alert(msgstatus)
   }, [msgstatus])
 
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setPaciente({ ...paciente, [name]: value })
   }
+
+
+  const handleChangeEmail = (e) => {
+    const { name, value } = e.target;
+    setPaciente({ ...email, [name]: value })
+  }
+
+
+  const handleChangeSenha = (e) => {
+    const { name, value } = e.target;
+    setPaciente({ ...senha, [name]: value })
+  }
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -90,42 +117,40 @@ export default function Cadastro() {
           <form onSubmit={handleSubmit}>
             <div className='nome'>
               <label for="idNome">Nome</label>
-              <input type="text" name='nome' id='idNome' required value={paciente.nm_paciente} onChange={handleChange} />
+              <input type="text" name='nm_paciente' id='idNome' required value={paciente.nm_paciente} onChange={handleChange} />
             </div>
             <div className='cpf'>
               <label for="idCpf">CPF</label>
-              <input type="text" id="cpf" name="idCpf" required  value={paciente.nr_cpf} onChange={handleChange}/>
+              <input type="text" id="cpf" name="nr_cpf" required  value={paciente.nr_cpf} onChange={handleChange}/>
             </div>
             <div className='rg'>
               <label for="idRg">RG</label>
-              <input type="text" id="rg" name="idRg" required  value={paciente.nr_rg} onChange={handleChange} />
+              <input type="text" id="rg" name="nr_rg" required  value={paciente.nr_rg} onChange={handleChange} />
             </div>
             <div className='sexoBiologico'>
               <label for="sexoBiologicoID">Sexo Biológico:</label>
-              <select id="sexoBiologico" name="sexoBiologicoID" required value={paciente.fl_sexo_biologico} onChange={handleChange}>
-                <option value="masculino">M</option>
-                <option value="feminino">F</option>
-                <option value="outro">I</option>
+              <select id="sexoBiologico" name="fl_sexo_biologico" required value={paciente.fl_sexo_biologico} onChange={handleChange}>
+                <option value="M">M</option>
+                <option value="F">F</option>
+                <option value="I">I</option>
               </select>
             </div>
             <div className='altura'>
               <label for="alturaID">Altura (cm):</label>
-              <input type="number" id="altura" name="alturaId" required  value={paciente.nr_altura} onChange={handleChange}/>
+              <input type="number" id="altura" name="nr_altura" required  value={paciente.nr_altura} onChange={handleChange}/>
             </div>
             <div className='peso'>
               <label for="pesoID">Peso (kg):</label>
-              <input type="number" id="peso" name="peso" required value={paciente.nr_rg} onChange={handleChange} />
+              <input type="number" id="peso" name="nr_peso" required value={paciente.nr_peso} onChange={handleChange} />
             </div>
-
-
-            {/* <div className='email'>
+            <div className='email'>
               <label for="idEmail">Endereço de email</label>
-              <input type="email" name='email' id='idEmail' required />
+              <input type="email" name='ds_email' id='idEmail' required value={email.ds_email} onChange={handleChangeEmail} />
             </div>
             <div className='senha'>
               <label for="idSenha">Senha</label>
-              <input type="password" name='senha' id='idSenha' required />
-            </div> */}
+              <input type="password" name='cd_senha' id='idSenha' required value={senha.cd_senha} onChange={handleChangeSenha} />
+            </div>
             <button className='botaoLogin'>CADASTRAR</button>
           </form>
         </div>
